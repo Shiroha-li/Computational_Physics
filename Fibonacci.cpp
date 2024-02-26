862cdb11651c1163dc3dd6ca98addad3559a8201
Shiroha-li <2497704063@qq.com>
2024-02-26 17:19:12 +0800

    Exercise of Week #2 :Writing a code for Fibonacci Numbers Fn

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -4,13 +4,13 @@ int main()
 {
     int index [3] ={7,15,31};
     int Fibonacci [31] = {0};
-    Fibonacci[0] = 1;
+    Fibonacci[0] = 0;
     Fibonacci[1] = 1;
     for (int i = 2;i < 31;i++){
         Fibonacci[i] = Fibonacci[i-1] + Fibonacci[i-2];
     }
     for (int i = 0;i < 3;i++){
-        cout << "F_" << index[i] << " = " << Fibonacci[index[i]-1] << "\n";
+        cout << "F_" << index[i] << " = " << Fibonacci[index[i]] << "\n";
     }
     return 0;
 }
