2d72862aab7e32bda7168b77305d4f2955ab4a12
Shiroha-li <137030194+Shiroha-li@users.noreply.github.com>
2024-04-01 00:45:22 +0800

    Delete Ising_Spin/Square_Lattice/build directory

--- Ising_Spin/Square_Lattice/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shiroha/Workspace/cp/Ising_Spin/Square_Lattice
-# Build directory: /home/shiroha/Workspace/cp/Ising_Spin/Square_Lattice/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(UnitTests1 "UnitTests1_catch2")
-set_tests_properties(UnitTests1 PROPERTIES  _BACKTRACE_TRIPLES "/home/shiroha/Workspace/cp/Ising_Spin/Square_Lattice/CMakeLists.txt;23;add_test;/home/shiroha/Workspace/cp/Ising_Spin/Square_Lattice/CMakeLists.txt;0;")
-subdirs("_deps/catch2-build")
